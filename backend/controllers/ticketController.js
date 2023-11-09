8731be729ba3e19f67aaab587772682534157041
muratozbk <slipstream11@yandex.com>
2023-11-09 18:02:14 +0300

    get and create ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,19 +7,49 @@ const Ticket = require('../models/ticketModel');
 //@route GET /api/tickets
 //@access private
 const getTickets = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id)
 
-    res.status(200).json({ message: 'getTickets' });
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+    const tickets = await Ticket.find({ user: req.user.id })
+
+    res.status(200).json(tickets);
 });
 
 //@desc create newtickets
 //@route POST /api/tickets
 //@access private
 const createTicket = asyncHandler(async (req, res) => {
+    const { product, description } = req.body;
 
-    res.status(200).json({ message: 'createTicket' });
+    if (!product || !description) {
+        res.status(404)
+        throw new Error('Please add a product and description')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: 'new'
+    })
+
+    res.status(201).json(ticket);
 });
 
 module.exports = {
     getTickets,
     createTicket
 }
+
+// 136

6ed2feed18320f927d9f8372e35f5610373b85e9
muratozbk <slipstream11@yandex.com>
2023-11-13 19:27:53 +0300

    close ticket

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import { UseSelector, useDispatch, useSelector } from "react-redux";
-import { getTicket, reset } from "../features/tickets/ticketSlice";
+import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function Ticket() {
@@ -12,6 +12,7 @@ function Ticket() {
 
     const params = useParams();
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { ticketId } = useParams()
 
     useEffect(() => {
@@ -22,6 +23,13 @@ function Ticket() {
         dispatch(getTicket(ticketId))
         //eslint-disable-next-line
     }, [isError, message, ticketId]);
+
+    //Close ticket
+    const onTicketClose = () => {
+        dispatch(closeTicket(ticketId));
+        toast.success('Ticket closed')
+        navigate('/tickets')
+    }
 
     if (isLoading) {
         return <Spinner />
@@ -42,15 +50,21 @@ function Ticket() {
                 </h2>
                 <h3>Date Submitted: {new Date(ticket.createdAt).
                     toLocaleString('en-US')}</h3>
+                <h3>Product: {ticket.product} </h3>
                 <hr />
                 <div className="ticket-desc">
                     <h3>Description of Issue</h3>
                     <p>{ticket.description} </p>
                 </div>
             </header>
+
+            {ticket.status !== 'closed' && (
+                <button className="btn btn-block btn-danger"
+                    onClick={onTicketClose}>
+                    Close Ticked</button>
+            )}
         </div>
     );
 }
-
+// 145
 export default Ticket;
-// 144
